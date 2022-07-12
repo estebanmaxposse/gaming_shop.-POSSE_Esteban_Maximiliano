24833f013a40385db65e857402c1b52f86906ae8
estebanmaxposse <estebanposse98@gmail.com>
2022-07-11 23:04:13 -0300

    added itemcount functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import NavBar from './components/NavBar';
-import HomeCarousel from './components/HomeCarousel';
+// import HomeCarousel from './components/HomeCarousel';
 import ItemListContainer from './components/ItemListContainer';
+import ItemCountModal from './components/ItemCountModal';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
        <NavBar/>
-       <HomeCarousel/>
+       {/* <HomeCarousel/> */}
        <ItemListContainer/>
+       <ItemCountModal/>
     </div>
   );
 }
---
+++ src/components/ItemCountModal.js
@@ -0,0 +1,47 @@
+import React, {useState} from 'react';
+import { Button, Modal, Container, Row, Col, Image } from 'react-bootstrap';
+import testGame from '../img/game_mario_kart_8_box.png';
+import ItemCount from './ItemCount'
+
+const ItemCountModal = () => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    
+    return (
+        <div>
+            <Button onClick={handleShow}>Toggle Item Count Modal</Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Item Count Test</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Container fluid>
+                        <Row>
+                            <Col>
+                                <Image fluid src={testGame}/>
+                            </Col>
+                            <Col>
+                                <h5>Mario Kart 8 Deluxe</h5>
+                                <p>$60,00</p>
+                                <ItemCount stock = {11} initial = {1}/>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    );
+};
+
+export default ItemCountModal;
