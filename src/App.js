5937a73133e6e24e847bc55aa61f0341b9540b68
estebanmaxposse <estebanposse98@gmail.com>
2022-07-27 21:29:13 -0300

    added cart context and basic cart functions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Homepage from "./components/Homepage";
@@ -7,24 +8,26 @@ import Categories from "./components/Categories";
 import ItemDetailContainer from "./components/ItemDetailContainer";
 import Cart from "./components/Cart";
 import Favicon from "react-favicon";
-import "./App.scss";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NoMatch from "./components/NoMatch";
+import CartProvider from "./contexts/CartContext";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <Favicon url="https://i.imgur.com/y61iWez.png" />
       <BrowserRouter>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/category/:categoryID" element={<Categories />} />
-          <Route path="/detail/:itemDetailID" element={<ItemDetailContainer />} />
-          <Route path="*" element={<NoMatch />} />
-        </Routes>
-        <Footer />
+        <CartProvider>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/category/:categoryID" element={<Categories />} />
+            <Route path="/detail/:itemDetailID" element={<ItemDetailContainer />} />
+            <Route path="*" element={<NoMatch />} />
+          </Routes>
+          <Footer />
+        </CartProvider>
       </BrowserRouter>
     </div>
   );
